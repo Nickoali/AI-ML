1fc4351610ac712d3927a91e106b67a0a041220c
Nickoali <123254920+Nickoali@users.noreply.github.com>
2024-08-21 21:15:28 -0500

    init

---
+++ test.py
@@ -0,0 +1,11 @@
+import random
+
+print("hrethrg")
+
+
+user_input = input("Enter something: ")
+
+for _ in range(10):
+    print(random.randint(1, 100))
+
+for _ in range(10): print(random.randint(1, 100))
